0c8879e0cdbddeed2d0860a894ce77b1f421cc66
Nathan Pepper <MayTheSForceBeWithYou@gmail.com>
2020-01-23 00:20:00 -0800

    Reordered app and added to org setup script and readme

--- scripts/shell/org_setup.sh
+++ scripts/shell/org_setup.sh
@@ -5,3 +5,5 @@
 # attempts should be made to isolate common scripts into their own files
 
 sfdx force:org:create -s -a sfdc-depl-mgr -f config/project-scratch-def.json
+sfdx force:source:push
+sfdx force:user:permset:assign -n SalesforceDeploymentManager_Admin
