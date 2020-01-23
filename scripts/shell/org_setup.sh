41e723731698b253ebccfad85e44b6ac7b961715
Nathan Pepper <MayTheSForceBeWithYou@gmail.com>
2020-01-23 00:11:37 -0800

    Good first night's work

---
+++ scripts/shell/org_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+# This setup script will start out with just sfdx commands
+# which should be executable on any terminal
+# Should future developments include shell specific commands,
+# attempts should be made to isolate common scripts into their own files
+
+sfdx force:org:create -s -a sfdc-depl-mgr -f config/project-scratch-def.json
