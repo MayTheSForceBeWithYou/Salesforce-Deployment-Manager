de7f0ed93cbaa630f52c33820f96eab5371b7534
Nathan Pepper <MayTheSForceBeWithYou@gmail.com>
2020-01-27 20:47:10 -0800

    Got package.xml generating from Quick Action

--- force-app/main/default/aura/GeneratePackageAuraAction/GeneratePackageAuraActionController.js
+++ force-app/main/default/aura/GeneratePackageAuraAction/GeneratePackageAuraActionController.js
@@ -1,5 +1,40 @@
 ({
     initToWinIt : function(component, event, helper) {
-        
+        // Call that Apex method to get this package.xml saved!
+        let action = component.get('c.attachXmlToDeployment'),
+            recordId = component.get('v.recordId');
+        action.setParams({
+            deploymentId: recordId
+        });
+        action.setCallback(this, function(response) {
+            let closeQuickAction = $A.get("e.force:closeQuickAction"),
+                resultsToast = $A.get("e.force:showToast"),
+                state = response.getState();
+            var toastTitle, toastMessage;
+            
+            if (state === 'SUCCESS') {
+                let retVal = response.getReturnValue();
+                toastTitle = 'Success';
+                toastMessage = 'Successfully generated package.xml and attached to Deployment.';
+            } else {
+                let errors = response.getError();
+                console.log('State not SUCCESS:', state);
+                toastTitle = 'Error';
+                toastMessage = 'There was an error generating the package.xml.\n';
+                errors.forEach((err) => {
+                    toastMessage += err + '\n';
+                    console.log(err);
+                });
+            }
+            // Display results in toast
+            resultsToast.setParams({
+                "title": toastTitle,
+                "message": toastMessage
+            });
+            resultsToast.fire();
+            
+            closeQuickAction.fire();
+        });
+        $A.enqueueAction(action);
     }
 })
